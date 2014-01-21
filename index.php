14320e3b45d453d486362e851396815db468a003
Matt <matthew.behn@gmail.com>
2014-01-21 15:33:37 -0600

    First

---
+++ index.php
@@ -0,0 +1,21 @@
+<!doctype html>
+
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>The Bazaarvoice Conversations API with JSONP</title>
+        <link rel="stylesheet" type="text/css" href="style.css">
+        <script type="text/javascript" src="script.js"></script>
+        <style type="text/css">
+        </style>
+
+    </head>
+
+    <body>
+        <header>Reviews Page API Example w/JSON Callback</header>
+        <!-- Demonstration purposes only. Request API keys for your application at https://developer.bazaarvoice.com/apps/register -->
+        <script src="http://stg.api.bazaarvoice.com/data/reviews.json?apiversion=5.4&passkey=3mlxjav3n99oox8sdfdpd99m9&filter=productid:eq:data-gen-txeel5kxxv1rl5z0g7xv13vo5&callback=JSONPHandler&limit=6"></script>
+        <div id="review-container" name="reviewsContainer"></div>
+    </body>
+
+</html>
---
+++ script.js
@@ -0,0 +1,29 @@
+
+            function JSONPHandler(data) {
+                console.log(data);
+                for (i=0;i<data.Results.length;i++) {
+                    var review = document.createElement("div");
+                    review.className="review";
+                    var reviewTitle = document.createElement("h3");
+                    reviewTitle.className="review-title";
+                    if (!data.Results[i].Title) {
+                        data.Results[i].Title = "Review Title";
+                    }
+                    reviewTitle.innerHTML=data.Results[i].Title.toUpperCase();
+                    review.appendChild(reviewTitle);
+                    var reviewDate = document.createElement("h4");
+                    reviewDate.className="review-date";
+                    reviewDate.innerHTML=data.Results[i].SubmissionTime;
+                    review.appendChild(reviewDate);
+                    var reviewText = document.createElement("p");
+                    reviewText.className = "review-text";
+                    if (!data.Results[i].ReviewText) {
+                        data.Results[i].ReviewText = "Review Text";
+                    }
+                    review.appendChild(reviewText);
+                    reviewText.innerHTML = data.Results[i].ReviewText;
+                    console.log(review);
+                    document.body.appendChild(review);
+                }
+
+        }
