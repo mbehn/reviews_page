24b953c9b8f642428948938d3d0bdb0c2eafd3cb
Matt <matthew.behn@gmail.com>
2014-01-26 11:02:27 -0600

    changing directories

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
     <body>
         <header>Reviews Page API Example w/JSONP Callback</header>
         <!-- Demonstration purposes only. Request API keys for your application at https://developer.bazaarvoice.com/apps/register -->
-        <script src="http://stg.api.bazaarvoice.com/data/reviews.json?apiversion=5.4&passkey=3mlxjav3n99oox8sdfdpd99m9&filter=productid:eq:data-gen-txeel5kxxv1rl5z0g7xv13vo5&callback=JSONPHandler&limit=10"></script>
+        <script src="http://stg.api.bazaarvoice.com/data/reviews.json?apiversion=5.4&passkey=kuy3zj9pr3n7i0wxajrzj04xo&hascphotos=true&callback=JSONPHandler&Include=Products&limit=10"></script>
         <div id="review-container" name="reviewsContainer"></div>
     </body>
 
--- script.js
+++ script.js
@@ -25,10 +25,15 @@ function JSONPHandler(data) {
         productId.className="product";
         productId.innerHTML= data.Results[i].ProductId;
         review.appendChild(productId);
+        var productDescription = document.createElement("p");
+        productDescription.className="product-description";
+        var products = new Array();
+        products.push(data.Results);
+        console.log(products);
         var reviewerAge = document.createElement("h5");
         reviewerAge.className="reviwer-age";
         if (!data.Results[i].ContextDataValues.Age) {
-            reviewerAge.innerHTML = "Not Available";
+            reviewerAge.innerHTML = "";
         }else
         reviewerAge.innerHTML= data.Results[i].ContextDataValues.Age.ValueLabel;
         review.appendChild(reviewerAge);
@@ -39,8 +44,22 @@ function JSONPHandler(data) {
         }
         review.appendChild(reviewText);
         reviewText.innerHTML = data.Results[i].ReviewText;
+        var reviewImage = document.createElement("img");
+
+        if (!data.Results[i].Photos[0]) {
+            reviewImage.src="https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQDBQGDMwwKrrjyl5frVZhTV1qDP6u3YtPhFW_XM6zjdStHkm0"
+        } else{
+        reviewImage.src = data.Results[i].Photos[0].Sizes.normal.Url;
+        }
+        reviewImage.alt = "Image for review id " + data.Results[i].Id;
+        review.appendChild(reviewImage);
         console.log(review);
         document.body.appendChild(review);
     }
 
 }
+var url = document.URL;
+var enc_url = encodeURI(url);
+var dec_url = decodeURI(url);
+console.log(enc_url);
+console.log(dec_url);
